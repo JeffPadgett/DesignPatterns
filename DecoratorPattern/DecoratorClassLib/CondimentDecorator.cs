83b21f2e3808681aa96b70f6767d558545555e16
Jeffrey Padgett <JeffreyPadgett1@gmail.com>
2021-04-20 10:44:04 -0400

    fix

--- DecoratorPattern/DecoratorClassLib/CondimentDecorator.cs
+++ DecoratorPattern/DecoratorClassLib/CondimentDecorator.cs
@@ -5,6 +5,6 @@ namespace DecoratorClassLib
     public abstract class CondimentDecorator : Beverage 
     {
         protected Beverage beverage;
-        public override abstract string GetDescription();
+        public abstract string GetDescription();
     } 
 }

d5575e8632aba494041c08db26d1ef414d199dff
JeffPadgett <JeffreyPadgett1@gmail.com>
2021-05-03 11:31:29 -0400

    DI

--- HelloDI/HelloDISolution/HelloDI.Client/Program.cs
+++ HelloDI/HelloDISolution/HelloDI.Client/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.IO;
 using HelloDI.Lib;
+using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.Configuration.FileExtensions;
 using static System.Console;
 
 namespace HelloDI.Client
@@ -8,7 +11,17 @@ namespace HelloDI.Client
     {
         static void Main()
         {
-            IMessageWriter writer = new ConsoleMessageWriter();
+            //IMessageWriter writer = new ConsoleMessageWriter();
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string typename = configuration["messageWriter"];
+            Type type = Type.GetType(typename, throwOnError: true);
+
+            IMessageWriter writer = (IMessageWriter)Activator.CreateInstance(type);              
+                
             var salution = new Salution(writer);
             salution.WriteMessage();
             //WriteLine("Hello World!");
